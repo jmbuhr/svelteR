90b29d4bf92c59ce8193e30f86690f75bd7f0f7d
Jannik Buhr <jannik.m.buhr@gmail.com>
2021-02-07 22:25:04 +0100

    rebuild pkgdown site

--- R/include_svelte.R
+++ R/include_svelte.R
@@ -7,20 +7,21 @@
 #'
 #' @return Code that is inserted into the output document
 #' @export
-include_svelte <- function(name, path, props) {
+include_svelte <- function(name, path, props, self_contained = TRUE) {
   label <- knitr::opts_current$get("label")
   label <- stringr::str_remove_all(label, "\\W")
+  external <- ifelse(self_contained, "", "data-external='1'")
   knitr::asis_output(
     glue::glue(
 "
 <div id='{label}'></div>
-<link rel='stylesheet' href='{path}/public/global.css'>
-<link rel='stylesheet' href='{path}/public/build/bundle.css'>
+<link rel='stylesheet' href='{path}/public/global.css' {external}>
+<link rel='stylesheet' href='{path}/public/build/bundle.css' {external}>
 <script>
 function load{label}() {{
   window['{name}']('{label}', {as.character(jsonlite::toJSON(props))})
 }}
 </script>
-<script async onload='load{label}()' src='{path}/public/build/bundle.js'></script>
+<script async onload='load{label}()' src='{path}/public/build/bundle.js' {external}></script>
 "))
 }
