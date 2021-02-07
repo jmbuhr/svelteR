641729aadfd18834053e87320d7f279030ccf3c1
Jannik Buhr <jannik.m.buhr@gmail.com>
2021-02-07 18:26:26 +0100

    document priority of css rules

--- inst/svelte-app/src/App.svelte
+++ inst/svelte-app/src/App.svelte
@@ -1,10 +1,20 @@
 <script>
 	export let name;
 	import cat from "./cat.jpeg"
+
+  let count = 0;
+
+	function handleClick() {
+		count += 1;
+	}
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
+	<h2>Click this button:</h2>
+	<button on:click={handleClick}>
+	Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 	<img id="cat" src={cat} alt="A cat!"/>
 	<p>Cat image from wikipedia: <a href="https://en.wikipedia.org/wiki/Cat_senses">source</a> </p>
@@ -24,6 +34,11 @@
 		font-size: 4em;
 		font-weight: 100;
 	}
+	h2 {
+	  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
+		font-size: 4em;
+		font-weight: 100;
+  }
 
 	#cat {
 	  max-width: 70%;
