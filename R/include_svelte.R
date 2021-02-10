930fcf03daf5d86ada79f44197ec87bb2700da56
Jannik Buhr <jannik.m.buhr@gmail.com>
2021-02-10 12:28:04 +0100

    include only code for apps with a unique name closes #1

--- R/include_svelte.R
+++ R/include_svelte.R
@@ -19,17 +19,54 @@ include_svelte <- function(name, path, props, self_contained = TRUE) {
   label <- knitr::opts_current$get("label")
   label <- stringr::str_remove_all(label, "\\W")
   external <- ifelse(self_contained, "", "data-external='1'")
+
+  # check if an app with the same name is already included:
+  is_new_app <- add_app(name)
+
+  # add a div for the app
+  div <- "<div id='{label}'></div>"
+
+  # include code of new app
+  app_code <- ifelse(is_new_app,
+                     "<link rel='stylesheet' href='{path}/public/global.css' {external}>
+                      <link rel='stylesheet' href='{path}/public/build/bundle.css' {external}>
+                      <script async id='{name}' src='{path}/public/build/bundle.js' {external}></script>",
+                     "")
+
+  # register onload with the id of the script tag for the app
+  onload_code <- "
+  <script>
+  function load{label}() {{
+    window['{name}']('{label}', {as.character(jsonlite::toJSON(props))})
+  }}
+  document.getElementById('{name}').addEventListener('load', load{label})
+  </script>
+  "
+
   knitr::asis_output(
-    glue::glue(
-"
-<div id='{label}'></div>
-<link rel='stylesheet' href='{path}/public/global.css' {external}>
-<link rel='stylesheet' href='{path}/public/build/bundle.css' {external}>
-<script>
-function load{label}() {{
-  window['{name}']('{label}', {as.character(jsonlite::toJSON(props))})
-}}
-</script>
-<script async onload='load{label}()' src='{path}/public/build/bundle.js' {external}></script>
-"))
+    glue::glue(div,
+               app_code,
+               onload_code))
+}
+
+# check if an app with the same name is already included:
+# Global unique app counter
+.onLoad <- function(libname, pkgname) {
+  svelte_apps <<- new.env()
+}
+
+# add unique apps and return whether the app is new
+add_app <- function(name) {
+  if (exists("apps", where = svelte_apps)) {
+    if (name %in% svelte_apps$apps) {
+      new_app <- FALSE
+    } else {
+      svelte_apps$apps <- c(svelte_apps$apps, name)
+      new_app <- TRUE
+    }
+  } else {
+    svelte_apps$apps <- name
+    new_app <- TRUE
+  }
+  return(new_app)
 }
